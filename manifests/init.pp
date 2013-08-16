2a5f757c5fb2074f3a2561949b42e2527e5d9d49
Tommy Muehle <tm@move-elevator.de>
2013-08-16 22:22:57 +0200

    Refactor init class

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,29 +2,9 @@
 #
 # This is the main typo3 class
 #
-#
-# == Parameters
-#
-# Standard class parameters
-#
-# [*project*]
-#
-# [*version*]
-#
-# [*site_path*]
-#
 # == Author
 # Tommy Muehle
 #
-class typo3 (
-  	$project   	 = params_lookup( 'project' ),
-  	$version  	 = params_lookup( 'version' ),
-  	$site_path	 = params_lookup( 'site_path' ),
-  	
-  	$source_dir  = "/usr/share/typo3"
-  	
-) {
-
-	
+class typo3 (){
 
 }
