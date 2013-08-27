f4dd894e75577c14c06116ffe3fc14d6b354d046
Tommy Muehle <tm@move-elevator.de>
2013-08-27 21:20:46 +0200

    Add availability of git package for further use

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,21 @@
 # Tommy Muehle
 #
 class typo3 (){
-	
-  if ! defined(Package['wget']) {
-    package { 'wget': ensure => 'installed' }
+
+  if $::operatingsystem =~ /^(Debian|Ubuntu)$/ and versioncmp($::operatingsystemrelease, "12") < 0 {
+    $packages = [ "wget", "git-core" ]
+  } else {
+    $packages = [ "wget", "git" ]
   }
+
+  safepackage { $packages: ensure => "installed" }
 	
+}
+
+define safepackage ( $ensure = installed ) {
+
+  if !defined(Package[$title]) {
+    package { $title: ensure => $ensure }
+  }
+
 }
